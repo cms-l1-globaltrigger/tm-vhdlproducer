e67395dd48b2a302eaae9089b90ec3211f97feaa
hbergaue <hbergaue@4525493e-7705-40b1-a816-d608a930855b>
2015-09-14 11:28:54 +0000

    bug fixed in algo_mapping_rop.ja.vhd, updated instance_calo_condition_v2.ja.vhd
    git-svn-id: https://svn.cern.ch/reps/cactus/trunk/cactusprojects/utm/tmVhdlProducer@39153 4525493e-7705-40b1-a816-d608a930855b

--- jinjaTemplates/algo_mapping_rop.ja.vhd
+++ jinjaTemplates/algo_mapping_rop.ja.vhd
@@ -61,24 +61,24 @@ a_a_f <= algo_after_finor_mask;
 
 -- ==== Inserted by TME - begin =============================================================================================================
 
-algo_after_finor_mask_rop_int <= (
+algo_before_prescaler_rop_int <= (
 {%- for algoName in menu.reporter['index_sorted'] %}
 {%- set algoDict = menu.reporter['algoDict'][algoName] %}
 {%- set algoIndex = algoDict["index"] %}
 {%- if algoIndex in menu.reporter['m2a'][iMod]%}
 {%- set localAlgoIndex = menu.reporter['m2a'][iMod].index(algoIndex) %}
-{{algoIndex}} => a_a_f({{localAlgoIndex}}),
+{{algoIndex}} => a_b_p({{localAlgoIndex}}),
 {%-endif%}
 {%-endfor%}
 others => '0');
 
-algo_after_finor_mask_rop_int <= (
+algo_after_prescaler_rop_int <= (
 {%- for algoName in menu.reporter['index_sorted'] %}
 {%- set algoDict = menu.reporter['algoDict'][algoName] %}
 {%- set algoIndex = algoDict["index"] %}
 {%- if algoIndex in menu.reporter['m2a'][iMod]%}
 {%- set localAlgoIndex = menu.reporter['m2a'][iMod].index(algoIndex) %}
-{{algoIndex}} => a_a_f({{localAlgoIndex}}),
+{{algoIndex}} => a_a_p({{localAlgoIndex}}),
 {%-endif%}
 {%-endfor%}
 others => '0');
