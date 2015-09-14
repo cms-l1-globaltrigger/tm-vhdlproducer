52ff0c7a527a32d4ccebb040189bda1aae65356e
nrad <nrad@4525493e-7705-40b1-a816-d608a930855b>
2015-09-14 16:13:25 +0000

    Update on the VHDL producer templates: seperated template logic from jinja templates
    git-svn-id: https://svn.cern.ch/reps/cactus/trunk/cactusprojects/utm/tmVhdlProducer@39175 4525493e-7705-40b1-a816-d608a930855b

--- jinjaTemplates/subTemplates/signal_algorithm.ja.vhd
+++
@@ -1,6 +0,0 @@
-{%- for algoName in menu.reporter['index_sorted'] %}
-{%- set algoIndex = menu.reporter['algoDict'][algoName]["index"] %}
-{%- if algoIndex in menu.reporter['m2a'][iMod]%}
-    signal {{algoName}} : std_logic;
-{%-endif%}
-{%-endfor%}
