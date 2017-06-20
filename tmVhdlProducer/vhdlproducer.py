03df6edf32282a476fc060f12ae2417d38ac406d
arnold <arnold@4525493e-7705-40b1-a816-d608a930855b>
2017-06-20 13:57:24 +0000

    refs #1722: code refactoring
    git-svn-id: https://svn.cern.ch/reps/cactus/trunk/cactusprojects/utm/tmVhdlProducer@49229 4525493e-7705-40b1-a816-d608a930855b

--- tmVhdlProducer/vhdlproducer.py
+++ tmVhdlProducer/vhdlproducer.py
@@ -83,83 +83,22 @@ CustomFilters = {
     'mmhashn': murmurhash,
 }
 
-# HB 2016-09-29: used new files, to contains of the files are inserted in "algo_mapping_rop_tpl.vhd", "gtl_module_tpl.vhd" and "gtl_pkg_tpl.vhd"
-UsedTemplates = [
+ModuleTemplates = [
     'algo_index.vhd',
     'gtl_module_signals.vhd',
     'gtl_module_instances.vhd',
     'ugt_constants.vhd',
-    #'gtl_module.vhd',
-    #'ugt_constant_pkg.vhd',
-    #'algo_mapping_rop.vhd',
-    #'gtl_pkg.vhd',
-    'menu.json', # obsolete?
 ]
 
 # -----------------------------------------------------------------------------
 # Additional Helpers
 # -----------------------------------------------------------------------------
 
-def mkdir_p(path):
-    """Equivalent to bash's `mkdir -p' command. Creates a directory recusively
-    and ignores it if the path already exists."""
-    try:
+def makedirs(path):
+    """Creates a directory recusively, ignores it if the path already exists."""
+    logging.debug("creating directory: %s", path)
+    if not os.path.exists(path):
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass # ignore existing dir...
-
-def writeXmlMenu(filename, json_dir):
-    """Updates a XML menu file based on inforamtion from a JSON file (used to apply
-    a previously calculated algorithm distribution over multiple modules).
-    """
-    # Load mapping from JSON
-    with open(os.path.join(json_dir, 'menu.json')) as fp:
-        json_data = json.load(fp)
-
-    menu = tmTable.Menu()
-    scale = tmTable.Scale()
-    ext_signal = tmTable.ExtSignal()
-
-    logging.info("reading source XML menu file %s", filename)
-
-    message = tmTable.xml2menu(filename, menu, scale, ext_signal, False)
-    if message:
-        logging.error("{filename}: {message}".format(message))
-        raise RuntimeError(message)
-
-    logging.info("processing menu \"%s\" ... ", menu.menu["name"])
-
-    # Update menu information
-    logging.info("updating menu information...")
-    logging.info("uuid_menu     : %s", json_data["menu_uuid"])
-    logging.info("uuid_firmware : %s", json_data["firmware_uuid"])
-    logging.info("n_modules     : %s", json_data["n_modules"])
-
-    # Update menu information
-    menu.menu["uuid_menu"] = str(json_data["menu_uuid"])
-    menu.menu["uuid_firmware"] = str(json_data["firmware_uuid"])
-    menu.menu["n_modules"] = str(json_data["n_modules"])
-    menu.menu["is_valid"] = "1"
-
-    # Collect algorithm names
-    names = [algorithm["name"] for algorithm in menu.algorithms]
-
-    # Update algorithm
-    for name, index, module_id, module_index in json_data["algorithms"]:
-        algorithm = tmTable.Row()
-        id = names.index(name)
-        for k, v in menu.algorithms[id].iteritems():
-            algorithm[k] = v
-        algorithm["index"] = str(index)
-        algorithm["module_id"] = str(module_id)
-        algorithm["module_index"] = str(module_index)
-        menu.algorithms[id] = algorithm
-
-    target = os.path.join(json_dir, '{name}_m{n}.xml'.format(name=menu.menu['name'], n=menu.menu["n_modules"]))
-
-    logging.info("writing target XML menu file %s", target)
-    tmTable.menu2xml(menu, scale, ext_signal, target)
 
 # -----------------------------------------------------------------------------
 #  Template engines with custom loader environment.
@@ -185,63 +124,117 @@ class TemplateEngine(object):
 class VhdlProducer(object):
     """VHDL producer class."""
 
-    def __init__(self, searchpath, verbose=False):
-        self.verbose = verbose
+    def __init__(self, searchpath):
         self.VHDLProducerVersion = __all__[0]+__version__
         self.engine = TemplateEngine(searchpath)
-        self.directoryDict = {}
 
     def create_dirs(self, directory, n_modules):
         """Create directory tree for output."""
-        self.directoryDict = {
+        directories = {
             "vhdl" : os.path.join(directory, "vhdl"),
             "testvectors" : os.path.join(directory, "testvectors"),
             "xml" : os.path.join(directory, "xml"),
             "doc" : os.path.join(directory, "doc"),
         }
         for i in range(n_modules):
-            self.directoryDict["module_{i}".format(i=i)] = os.path.join(self.directoryDict['vhdl'], "module_{i}/src/".format(i=i))
-        if os.path.exists(self.directoryDict['vhdl']):
-            logging.warning("diectory `%s' already exists. Will be replaced.", self.directoryDict['vhdl'])
-            shutil.rmtree(self.directoryDict['vhdl'])
-        if os.path.exists(self.directoryDict['xml']):
-            logging.warning("directory `%s' already exists. Will be replaced.", self.directoryDict['xml'])
-            shutil.rmtree(self.directoryDict['xml'])
-        if os.path.exists(self.directoryDict['doc']):
-            logging.warning("directory `%s' already exists. Will be replaced.", self.directoryDict['doc'])
-            shutil.rmtree(self.directoryDict['doc'])
-
-        for directory in self.directoryDict:
-            mkdir_p(self.directoryDict[directory])
-            if self.verbose:
-                logging.info("created directory %s", self.directoryDict[directory])
+            module_id = "module_{i}".format(i=i)
+            directories[module_id] = os.path.join(directories['vhdl'], module_id, "src")
+        # Check for exisiting directories (TODO obsolete?)
+        for directory in directories:
+            if os.path.exists(directory):
+                logging.warning("directory `%s' already exists. Will be overwritten.", directory)
+                shutil.rmtree(directory)
+        # Create directries
+        for directory in directories:
+            makedirs(directories[directory])
+        return directories
 
     def write(self, collection, directory):
         """Write distributed modules (VHDL templates) to *directory*."""
 
         helper = vhdlhelper.MenuHelper(collection)
         logging.info("writing %s algorithms to %s module(s)", len(helper.algorithms), len(helper.modules))
-        ## Loop over algos and insert them in each module.
-        self.create_dirs(directory, len(collection)) # TODO still obfuscated init of self.directoryDict
-
-        writeJson = True # TODO
+        # Create directory tree
+        directories = self.create_dirs(directory, len(collection))
+        # Populate modules
         for module in helper.modules:
-            logging.info("writing template output:")
-            for template in UsedTemplates:
-                if template.endswith('.json'): # HACK use rather collection.dump(...)
-                    if not writeJson: continue
-                    writeJson = False
-                    filename = os.path.join(self.directoryDict["xml"], template)
-                else:
-                    module_id = "module_{id}".format(id=module.id)
-                    filename = os.path.join(self.directoryDict[module_id], template)
+            logging.info("writing output for module: %s", module.id)
+            for template in ModuleTemplates:
                 params = {
                     'menu': helper,
                     'module': module,
                 }
                 content = self.engine.render(template, params)
-                with open(filename, 'a') as fp:
+                module_id = "module_{id}".format(id=module.id)
+                filename = os.path.join(directories[module_id], template)
+                with open(filename, 'w') as fp:
                     fp.write(content)
                 logging.info("{template:<24}: {filename}".format(**locals()))
+
+        # Write JSON dump (TODO obsolete?)
+        params = {
+            'menu': helper,
+        }
+        content = self.engine.render('menu.json', params)
+        filename = os.path.join(directories['xml'], 'menu.json')
+        makedirs(os.path.dirname(filename)) # Create path if required
+        with open(filename, 'w') as fp:
+            fp.write(content)
+
+    def writeXmlMenu(self, filename, json_dir):
+        """Updates a XML menu file based on inforamtion from a JSON file (used to apply
+        a previously calculated algorithm distribution over multiple modules).
+        """
+        # TODO
+        # Load mapping from JSON
+        with open(os.path.join(json_dir, 'menu.json')) as fp:
+            json_data = json.load(fp)
+
+        menu = tmTable.Menu()
+        scale = tmTable.Scale()
+        ext_signal = tmTable.ExtSignal()
+
+        logging.info("reading source XML menu file %s", filename)
+
+        message = tmTable.xml2menu(filename, menu, scale, ext_signal, False)
+        if message:
+            logging.error("{filename}: {message}".format(message))
+            raise RuntimeError(message)
+
+        logging.info("processing menu \"%s\" ... ", menu.menu["name"])
+
+        # Update menu information
+        logging.info("updating menu information...")
+        logging.info("uuid_menu     : %s", json_data["menu_uuid"])
+        logging.info("uuid_firmware : %s", json_data["firmware_uuid"])
+        logging.info("n_modules     : %s", json_data["n_modules"])
+
+        # Update menu information
+        menu.menu["uuid_menu"] = str(json_data["menu_uuid"])
+        menu.menu["uuid_firmware"] = str(json_data["firmware_uuid"])
+        menu.menu["n_modules"] = str(json_data["n_modules"])
+        menu.menu["is_valid"] = "1"
+
+        # Collect algorithm names
+        names = [algorithm["name"] for algorithm in menu.algorithms]
+
+        # Update algorithm
+        for name, index, module_id, module_index in json_data["algorithms"]:
+            algorithm = tmTable.Row()
+            id_ = names.index(name)
+            # Copy attributes
+            for k, v in menu.algorithms[id_].iteritems():
+                algorithm[k] = v
+            # Update attributes
+            algorithm["index"] = str(index)
+            algorithm["module_id"] = str(module_id)
+            algorithm["module_index"] = str(module_index)
+            menu.algorithms[id_] = algorithm
+
+        target = os.path.join(json_dir, '{name}_m{n}.xml'.format(name=menu.menu['name'], n=menu.menu["n_modules"]))
+
+        logging.info("writing target XML menu file %s", target)
+        tmTable.menu2xml(menu, scale, ext_signal, target)
+
 
 # eof
