87976f3dbfc5e3ce2d03901c6d00ec464c7f2f16
arnold <arnold@4525493e-7705-40b1-a816-d608a930855b>
2018-03-23 11:09:06 +0000

    refs #1722: improved handling of firmware distribution argument (--dist)
    git-svn-id: https://svn.cern.ch/reps/cactus/trunk/cactusprojects/utm/tmVhdlProducer@50532 4525493e-7705-40b1-a816-d608a930855b

--- tmVhdlProducer/vhdlproducer.py
+++ tmVhdlProducer/vhdlproducer.py
@@ -183,7 +183,7 @@ class VhdlProducer(object):
         with open(filename, 'w') as fp:
             fp.write(content)
 
-    def writeXmlMenu(self, filename, json_dir, fwdist):
+    def writeXmlMenu(self, filename, json_dir, dist=1):
         """Updates a XML menu file based on inforamtion from a JSON file (used to apply
         a previously calculated algorithm distribution over multiple modules).
         Returns path and filename of created XML menu.
@@ -234,7 +234,7 @@ class VhdlProducer(object):
             algorithm["module_index"] = str(module_index)
             menu.algorithms[id_] = algorithm
 
-        target = os.path.join(json_dir, '{name}{n}.xml'.format(name=menu.menu['name'], n=fwdist))
+        target = os.path.join(json_dir, '{name}-d{dist}.xml'.format(name=menu.menu['name'], dist=dist))
 
         logging.info("writing target XML menu file %s", target)
         tmTable.menu2xml(menu, scale, ext_signal, target)
