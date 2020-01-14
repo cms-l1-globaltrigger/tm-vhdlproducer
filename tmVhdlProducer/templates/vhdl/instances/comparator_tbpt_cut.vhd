4da71d8701aeda4fef57dbd80db262efb88522ea
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-01-14 14:04:39 +0100

    updated for tbpt

--- tmVhdlProducer/templates/vhdl/instances/comparator_tbpt_cut.vhd
+++ tmVhdlProducer/templates/vhdl/instances/comparator_tbpt_cut.vhd
@@ -1,5 +1,5 @@
-{%- block instantiate_condition_tbpt_cut %}
-    comp_tbpt_{{ obj1|lower }}_{{ obj2|lower }}_bx{{ bx1 }}_bx{{ bx2 }}_0x{{ limit_l|lower }}_i: entity work.conditions_corr_cuts
+{%- block instantiate_comparator_tbpt_cut %}
+    comp_tbpt_{{ obj1|lower }}_{{ obj2|lower }}_bx_{{ bx1 }}_bx_{{ bx2 }}_0x{{ limit_l|lower }}_i: entity work.comparators_corr_cuts
         generic map(
             N_{{ obj1|upper }}_OBJECTS, N_{{ obj2|upper }}_OBJECTS, ({{ obj1|lower }}_t,{{ obj2|lower }}_t), (bx({{ bx1|bx_dec }}),bx({{ bx2|bx_dec }})),
             {{ obj1|upper }}_{{ obj2|upper }}_TBPT_VECTOR_WIDTH, twoBodyPt, 
@@ -9,4 +9,4 @@
             lhc_clk, 
             tbpt_{{ obj1|lower }}_{{ obj2|lower }}(bx({{ bx1|bx_dec }}),bx({{ bx2|bx_dec }})), comp_tbpt_{{ obj1|lower }}_{{ obj2|lower }}_bx_{{ bx1 }}_bx_{{ bx2 }}_0x{{ limit_l|lower }}_0x{{ limit_l|lower }}
         );
-{% endblock instantiate_condition_tbpt_cut %}
+{% endblock instantiate_comparator_tbpt_cut %}
