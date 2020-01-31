a6cbb8de6cc9a1665f9ac6f0ffce2007c108cf70
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-01-31 09:54:47 +0100

    updated for instantiation of esums, minbias and asyms, updated version

--- tmVhdlProducer/templates/vhdl/instances/combinatorial_conditions_count_obj.vhd
+++ tmVhdlProducer/templates/vhdl/instances/combinatorial_conditions_count_obj.vhd
@@ -1,6 +1,6 @@
 {%- block instantiate_combinatorial_conditions_count_obj %}
   {%- set o1 = condition.objects[0] %}
     {%- with obj = condition.objects[0] %}
-        {{ condition.vhdl_signal }} => {%- include  "helper/helper_comb_and_count_signals_names.txt" %}(0);
+    {{ condition.vhdl_signal }} <= {%- include  "helper/helper_comb_and_count_signals_names.txt" %}(0);
     {%- endwith %}
 {%- endblock instantiate_combinatorial_conditions_count_obj %}
--- tmVhdlProducer/templates/vhdl/instances/combinatorial_conditions_esums.vhd
+++ tmVhdlProducer/templates/vhdl/instances/combinatorial_conditions_esums.vhd
@@ -1,6 +1,6 @@
 {%- block instantiate_combinatorial_conditions_esums %}
   {%- set o1 = condition.objects[0] %}
     {%- with obj = condition.objects[0] %}
-        {{ condition.vhdl_signal }} => {% include "helper/helper_comb_and_esums_signals_names.txt" %}(0);
+    {{ condition.vhdl_signal }} <= {% include "helper/helper_comb_and_esums_signals_names.txt" %}(0);
     {%- endwith %}
 {%- endblock instantiate_combinatorial_conditions_esums %}
