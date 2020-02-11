ce4ce12e2f94bb14fa932fce547551df987225e8
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-02-11 12:11:04 +0100

    bug fixed in template signal_condition.vhd

--- tmVhdlProducer/templates/vhdl/instances/signal_condition.vhd
+++ tmVhdlProducer/templates/vhdl/instances/signal_condition.vhd
@@ -1,4 +1,6 @@
 {%- block instantiate_signal_condition %}
+  {%- set o = condition.objects[0] %}
+    {%- if o.is_signal_type  %}
     {%- if o.type == 'CENT0' %}
         {%- set cent_bit = 0 %}
     {%- elif o.type == 'CENT1' %}  
@@ -16,5 +18,6 @@
     {%- elif o.type == 'CENT7' %}  
         {%- set cent_bit = 7 %}
     {%- endif %}  
-    {{ condition.vhdl_signal }} <= data.centrality(bx({{ bx|bx_dec }}))({{ cent_bit }});
+    {{ condition.vhdl_signal }} <= data.centrality(bx({{ o.bx|bx_dec }}))({{ cent_bit }});
+    {%- endif %}
 {%- endblock instantiate_signal_condition %}
