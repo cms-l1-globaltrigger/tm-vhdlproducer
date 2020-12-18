f13f0efea01b40816be51fbc8fa2367483c360a1
herbberg <herbert.bergauer@oeaw.ac.at>
2020-12-18 11:38:22 +0100

    deleted obsolete templates, added a new one and updated others

--- tmVhdlProducer/templates/vhdl/instances/calo_conditions_orm.vhd
+++ tmVhdlProducer/templates/vhdl/instances/calo_conditions_orm.vhd
@@ -51,19 +51,7 @@
     {%- set temp = isolationLUTList.pop(i) %}
   {%- endfor %}        
   {%- include "instances/object_cuts_calo_orm.vhd" %}
--- correlation cuts orm
-  {%- if condition.hasDeltaEtaOrm %}
-        diff_eta_orm_upper_limit_vector => X"{{ condition.deltaEtaOrm.upper|X08 }}", 
-        diff_eta_orm_lower_limit_vector => X"{{ condition.deltaEtaOrm.lower|X08 }}",
-  {%- endif %}        
-  {%- if condition.hasDeltaPhiOrm %}
-        diff_phi_orm_upper_limit_vector => X"{{ condition.deltaPhiOrm.upper|X08 }}", 
-        diff_phi_orm_lower_limit_vector => X"{{ condition.deltaPhiOrm.lower|X08 }}",
-  {%- endif %}        
-  {%- if condition.hasDeltaROrm %}
-        dr_orm_upper_limit_vector => X"{{ condition.deltaROrm.upper|X16 }}", 
-        dr_orm_lower_limit_vector => X"{{ condition.deltaROrm.lower|X16 }}",
-  {%- endif %}        
+  {%- include "instances/correlation_cuts_orm.vhd" %}
   {%- if condition.hasTwoBodyPt %}
 -- correlation cuts
         twobody_pt_cut => true, 
