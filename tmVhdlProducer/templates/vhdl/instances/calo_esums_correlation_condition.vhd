51d3b66f9dd4d8b27979951509e9cbe3aa09985b
bergauer <herbert.bergauer@oeaw.ac.at>
2021-02-09 09:52:39 +0100

    fixec typo

--- tmVhdlProducer/templates/vhdl/instances/calo_esums_correlation_condition.vhd
+++ tmVhdlProducer/templates/vhdl/instances/calo_esums_correlation_condition.vhd
@@ -2,7 +2,7 @@
 
 {% block entity %}work.correlation_conditions_calo{% endblock %}
 
-{%- block generic_map_end %}
+{%- block generic_map %}
 {{ super() }}
 -- correlation cuts
     {%- if condition.deltaPhi %}
