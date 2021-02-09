0bed6755549e5216c45c16a3c350f12ade2e61d0
Bernhard Arnold <bernhard.arnold@cern.ch>
2021-02-09 13:25:03 +0100

    refs #21

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name="tm-vhdlproducer",
-    version='2.9.1',
+    version='2.9.2',
     url="https://github.com/cms-l1-globaltrigger/tm-vhdlproducer",
     author="Bernhard Arnold",
     author_email="bernhard.arnold@cern.ch",
