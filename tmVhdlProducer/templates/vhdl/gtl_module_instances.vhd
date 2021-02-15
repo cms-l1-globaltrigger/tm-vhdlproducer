c18afb3d60a89a9da42e18dd360cf46a0cf394f3
bergauer <herbert.bergauer@oeaw.ac.at>
2021-02-15 11:55:14 +0100

    updated template for new ugt fw instance (obj_parameter.vhd)

--- tmVhdlProducer/templates/vhdl/gtl_module_instances.vhd
+++ tmVhdlProducer/templates/vhdl/gtl_module_instances.vhd
@@ -24,18 +24,19 @@
 -- External condition assignment
 
 {%- for condition in module.externalConditions %}
-{{ condition.vhdl_signal }} <= ext_cond_bx_{{ condition.objects[0].bx }}({{ condition.objects[0].externalChannelId }}); -- {{ condition.vhdl_signal }}
+    {{ condition.vhdl_signal }} <= ext_cond_bx_{{ condition.objects[0].bx }}({{ condition.objects[0].externalChannelId }}); -- {{ condition.vhdl_signal }}
 {%- endfor %}
--- Instantiations of muon charge correlations - only once for a certain Bx combination, if there is at least one DoubleMuon, TripleMuon, QuadMuon condition
+
+-- Instantiations of muon charge correlations - only once for a certain bx combination, if there is at least one DoubleMuon, TripleMuon, QuadMuon condition
 -- or muon-muon correlation condition.
 {% include "instances/muon_charge_correlations.vhd" %}
--- Instantiations of eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass) - once for every calo ObjectType in certain Bx used in correlation conditions
+-- Instantiations of eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass) - once for every calo object type in certain bx used in correlation conditions
 {% include "instances/correlation_conditions_eta_phi_conversion.vhd" %}
--- Instantiations of pt, eta, phi, cos-phi and sin-phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and b_tagging) - once for every ObjectType in certain Bx used in correlation conditions
+-- Instantiations of pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt) - once for every object type in certain bx used in correlation conditions
 {% include "instances/correlation_conditions_pt_eta_phi_cos_sin_phi.vhd" %}
--- Instantiations of differences for correlation conditions (used for DETA, DPHI, DR, mass and b_tagging) - once for correlation conditions with two ObjectTypes in certain Bxs
+-- Instantiations of differences for correlation conditions (used for DETA, DPHI, DR, mass and two-body pt) - once for correlation conditions with two object types in certain bxs
 {% include "instances/correlation_conditions_differences.vhd" %}
--- Instantiations of cosh-deta and cos-dphi LUTs for correlation conditions (used for mass and overlap_remover) - once for correlation conditions with two ObjectTypes in certain Bxs
+-- Instantiations of cosh deta and cos dphi LUTs for correlation conditions (used for mass) - once for correlation conditions with two object types in certain bxs
 {% include "instances/correlation_conditions_mass_cuts.vhd" %}
 
 -- Instantiations of conditions
--- tmVhdlProducer/templates/vhdl/instances/correlation_conditions_pt_eta_phi_cos_sin_phi.vhd
+++ tmVhdlProducer/templates/vhdl/instances/correlation_conditions_pt_eta_phi_cos_sin_phi.vhd
@@ -1,34 +1,33 @@
-{%- block object_loop scoped %}
-  {%- for o in module.correlationObjects %}
-    {%- if o.is_calo_type %}
-    {{ o.type | lower }}_bx_{{ o.bx }}_data_l: for i in 0 to NR_{{ o.type | upper }}_OBJECTS-1 generate
-        {{ o.type | lower }}_bx_{{ o.bx }}_pt_vector(i)({{ o.type | upper }}_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR({{ o.type | upper }}_PT_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.et_high downto D_S_I_{{ o.type | upper }}_V2.et_low))), {{ o.type | upper }}_PT_VECTOR_WIDTH);
-        {{ o.type | lower }}_bx_{{ o.bx }}_eta_integer(i) <= CONV_INTEGER(signed({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.eta_high downto D_S_I_{{ o.type | upper }}_V2.eta_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_phi_integer(i) <= CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low));
-        {{ o.type | lower }}_bx_{{ o.bx }}_cos_phi(i) <= CALO_COS_PHI_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi(i) <= CALO_SIN_PHI_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_conv_cos_phi(i) <= MUON_COS_PHI_LUT({{ o.type | lower }}_bx_{{ o.bx }}_phi_conv_2_muon_phi_integer(i));
-        {{ o.type | lower }}_bx_{{ o.bx }}_conv_sin_phi(i) <= MUON_SIN_PHI_LUT({{ o.type | lower }}_bx_{{ o.bx }}_phi_conv_2_muon_phi_integer(i));
-    end generate {{ o.type | lower }}_bx_{{ o.bx }}_data_l;
-    {%- elif o.is_muon_type %}
-    {{ o.type | lower }}_bx_{{ o.bx }}_data_l: for i in 0 to NR_{{ o.type | upper }}_OBJECTS-1 generate
-        {{ o.type | lower }}_bx_{{ o.bx }}_pt_vector(i)({{ o.type | upper }}_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR({{ o.type | upper }}_PT_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.pt_high downto D_S_I_{{ o.type | upper }}_V2.pt_low))), {{ o.type | upper }}_PT_VECTOR_WIDTH);
-        {{ o.type | lower }}_bx_{{ o.bx }}_upt_vector(i)({{ o.type | upper }}_UPT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR({{ o.type | upper }}_UPT_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.upt_high downto D_S_I_{{ o.type | upper }}_V2.upt_low))), {{ o.type | upper }}_UPT_VECTOR_WIDTH);
-        {{ o.type | lower }}_bx_{{ o.bx }}_eta_integer(i) <= CONV_INTEGER(signed({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.eta_high downto D_S_I_{{ o.type | upper }}_V2.eta_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_phi_integer(i) <= CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low));
-        {{ o.type | lower }}_bx_{{ o.bx }}_cos_phi(i) <= MUON_COS_PHI_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi(i) <= MUON_SIN_PHI_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(i)(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low)));
-    end generate {{ o.type | lower }}_bx_{{ o.bx }}_data_l;
-    {%- elif o.is_esums_type %}
-    {{ o.type | lower }}_bx_{{ o.bx }}_data_l: for i in 0 to NR_{{ o.type | upper }}_OBJECTS-1 generate
-        {{ o.type | lower }}_bx_{{ o.bx }}_pt_vector(0)({{ o.type | upper }}_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR({{ o.type | upper }}_PT_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(D_S_I_{{ o.type | upper }}_V2.et_high downto D_S_I_{{ o.type | upper }}_V2.et_low))), {{ o.type | upper }}_PT_VECTOR_WIDTH);
-        {{ o.type | lower }}_bx_{{ o.bx }}_phi_integer(0) <= CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low));
-        {{ o.type | lower }}_bx_{{ o.bx }}_cos_phi(0) <= CALO_COS_PHI_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi(0) <= CALO_SIN_PHI_LUT(CONV_INTEGER({{ o.type | lower }}_bx_{{ o.bx }}(D_S_I_{{ o.type | upper }}_V2.phi_high downto D_S_I_{{ o.type | upper }}_V2.phi_low)));
-        {{ o.type | lower }}_bx_{{ o.bx }}_conv_cos_phi(0) <= MUON_COS_PHI_LUT({{ o.type | lower }}_bx_{{ o.bx }}_phi_conv_2_muon_phi_integer(0));
-        {{ o.type | lower }}_bx_{{ o.bx }}_conv_sin_phi(0) <= MUON_SIN_PHI_LUT({{ o.type | lower }}_bx_{{ o.bx }}_phi_conv_2_muon_phi_integer(0));
-    end generate {{ o.type | lower }}_bx_{{ o.bx }}_data_l;
-    {%- endif %}
-  {%- endfor %}
-{%- endblock object_loop %}
-{# eof #}
+{%- for o in module.correlationObjects %}
+    {{ o.type | lower }}_bx_{{ o.bx }}_parameter_i: work.obj_parameter
+        generic map(
+            nr_obj => NR_{{ o.type | upper }}_OBJECTS,
+            type_obj => {{ o.type | upper }}_TYPE
+        )
+        port map(
+        {%- if o.is_calo_type %}
+            calo => {{ o.type | lower }}_bx_{{ o.bx }},
+        {%- elif o.is_muon_type %}
+            muon => {{ o.type | lower }}_bx_{{ o.bx }},
+        {%- elif o.is_esums_type %}
+            esums => {{ o.type | lower }}_bx_{{ o.bx }},
+        {%- endif %}
+            phi_conv_2_muon_phi_integer => {{ o.type | lower }}_bx_{{ o.bx }}_phi_conv_2_muon_phi_integer,
+            pt_vector => {{ o.type | lower }}_bx_{{ o.bx }}_pt_vector
+        {%- if o.is_muon_type %}
+            upt_vector => {{ o.type | lower }}_bx_{{ o.bx }}_upt_vector
+        {%- endif %}
+        {%- if not o.is_esums_type %}
+            eta_integer => {{ o.type | lower }}_bx_{{ o.bx }}_eta_integer,
+        {%- endif %}
+            phi_integer => {{ o.type | lower }}_bx_{{ o.bx }}_phi_integer,
+            cos_phi => {{ o.type | lower }}_bx_{{ o.bx }}_cos_phi,
+            sin_phi => {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi,
+        {%- if not o.is_muon_type %}
+            conv_cos_phi => {{ o.type | lower }}_bx_{{ o.bx }}_conv_cos_phi,
+            conv_sin_phi => {{ o.type | lower }}_bx_{{ o.bx }}_conv_sin_phi
+        {%- endif %}
+        );
+--
+
+{%- endfor %}
