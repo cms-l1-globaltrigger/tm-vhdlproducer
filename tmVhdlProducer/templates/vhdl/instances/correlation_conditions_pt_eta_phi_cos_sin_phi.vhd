c5b10cf6c54f2ff6fbffa996e2c6610471635ec1
bergauer <herbert.bergauer@oeaw.ac.at>
2021-02-16 09:35:55 +0100

    bug fixed

--- tmVhdlProducer/templates/vhdl/instances/correlation_conditions_pt_eta_phi_cos_sin_phi.vhd
+++ tmVhdlProducer/templates/vhdl/instances/correlation_conditions_pt_eta_phi_cos_sin_phi.vhd
@@ -1,5 +1,5 @@
 {%- for o in module.correlationObjects %}
-    {{ o.type | lower }}_bx_{{ o.bx }}_parameter_i: work.obj_parameter
+    {{ o.type | lower }}_bx_{{ o.bx }}_parameter_i: entity work.obj_parameter
         generic map(
             nr_obj => NR_{{ o.type | upper }}_OBJECTS,
             type_obj => {{ o.type | upper }}_TYPE
@@ -12,20 +12,24 @@
         {%- elif o.is_esums_type %}
             esums => {{ o.type | lower }}_bx_{{ o.bx }},
         {%- endif %}
+        {%- if not o.is_muon_type %}
             phi_conv_2_muon_phi_integer => {{ o.type | lower }}_bx_{{ o.bx }}_phi_conv_2_muon_phi_integer,
-            pt_vector => {{ o.type | lower }}_bx_{{ o.bx }}_pt_vector
+        {%- endif %}
+            pt_vector => {{ o.type | lower }}_bx_{{ o.bx }}_pt_vector,
         {%- if o.is_muon_type %}
-            upt_vector => {{ o.type | lower }}_bx_{{ o.bx }}_upt_vector
+            upt_vector => {{ o.type | lower }}_bx_{{ o.bx }}_upt_vector,
         {%- endif %}
         {%- if not o.is_esums_type %}
             eta_integer => {{ o.type | lower }}_bx_{{ o.bx }}_eta_integer,
         {%- endif %}
             phi_integer => {{ o.type | lower }}_bx_{{ o.bx }}_phi_integer,
             cos_phi => {{ o.type | lower }}_bx_{{ o.bx }}_cos_phi,
-            sin_phi => {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi,
         {%- if not o.is_muon_type %}
+            sin_phi => {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi,
             conv_cos_phi => {{ o.type | lower }}_bx_{{ o.bx }}_conv_cos_phi,
             conv_sin_phi => {{ o.type | lower }}_bx_{{ o.bx }}_conv_sin_phi
+        {%- else %}
+            sin_phi => {{ o.type | lower }}_bx_{{ o.bx }}_sin_phi
         {%- endif %}
         );
 --
