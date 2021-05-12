c00b009c716a137d7ba4cee40f860863ba2d581b
Bernhard Arnold <bernhard.arnold@cern.ch>
2021-05-12 15:48:39 +0200

    version 2.9.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name="tm-vhdlproducer",
-    version='2.9.3',
+    version='2.9.4',
     url="https://github.com/cms-l1-globaltrigger/tm-vhdlproducer",
     author="Bernhard Arnold",
     author_email="bernhard.arnold@cern.ch",
@@ -22,8 +22,8 @@ setup(
     },
     install_requires=[
         'Jinja2',
-        'tm-python @ git+https://github.com/cms-l1-globaltrigger/tm-python@0.8.1',
-        'tm-reporter @ git+https://github.com/cms-l1-globaltrigger/tm-reporter@2.8.1'
+        'tm-python @ git+https://github.com/cms-l1-globaltrigger/tm-python@0.8.2',
+        'tm-reporter @ git+https://github.com/cms-l1-globaltrigger/tm-reporter@2.8.2'
     ],
     entry_points={
         'console_scripts': [
