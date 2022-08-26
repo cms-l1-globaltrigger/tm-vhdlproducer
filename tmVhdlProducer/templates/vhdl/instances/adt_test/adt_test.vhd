1a3f86bdaebec7255c17dba19709ebb0e619504e
herbberg <herbert.bergauer@oeaw.ac.at>
2022-08-26 11:28:07 +0200

    updated port map

--- tmVhdlProducer/templates/vhdl/instances/adt_test/adt_test.vhd
+++ tmVhdlProducer/templates/vhdl/instances/adt_test/adt_test.vhd
@@ -4,16 +4,16 @@ cond_{{ condition.objects[0].name | lower}}_i: entity work.adt
     port map(
         lhc_clk,
 --         clk240: in std_logic;
-        bx_data.mu,
-        bx_data.eg,
-        bx_data.jet,
-        bx_data.tau,
-        bx_data.ett,
-        bx_data.htt,
-        bx_data.etm,
-        bx_data.htm,
-        bx_data.ettem,
-        bx_data.etmhf,
+        bx_data(2).mu,
+        bx_data(2).eg,
+        bx_data(2).jet,
+        bx_data(2).tau,
+--         bx_data(2).ett,
+--         bx_data(2).htt,
+--         bx_data(2).etm,
+--         bx_data(2).htm,
+--         bx_data(2).ettem,
+--         bx_data(2).etmhf,
         {{ condition.vhdl_signal }}
     );
-Footer
+
