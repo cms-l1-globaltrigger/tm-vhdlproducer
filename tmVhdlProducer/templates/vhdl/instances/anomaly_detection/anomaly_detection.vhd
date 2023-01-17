a13aebc21325df39ed931c3ecdcfed5cefef565d
Herbert Bergauer <Herbert.Bergauer@oeaw.ac.at>
2023-01-17 10:29:11 +0100

    fixed typo

--- tmVhdlProducer/templates/vhdl/instances/anomaly_detection/anomaly_detection.vhd
+++ tmVhdlProducer/templates/vhdl/instances/anomaly_detection/anomaly_detection.vhd
@@ -5,8 +5,8 @@ cond_{{ condition.objects[0].name | lower}}_i: entity work.adt_wrapper
     port map(
         lhc_clk,
         bx_data.mu(2),
-        bx_data.jet(2),
         bx_data.eg(2),
+        bx_data.jet(2),
         bx_data.tau(2),
         bx_data.ett(2),
         bx_data.htt(2),
