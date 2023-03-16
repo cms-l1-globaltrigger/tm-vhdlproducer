19453d1c6f607a8cef2bc53432cc1e5b9d726a4f
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2023-03-16 12:30:50 +0100

    updated for tm-python 0.11.1 and tm-reporter 2.11.2

--- setup.py
+++ setup.py
@@ -24,8 +24,8 @@ setup(
     install_requires=[
         'Jinja2==2.11.*',
         'markupsafe==2.0.*',
-        'tm-python @ git+https://github.com/cms-l1-globaltrigger/tm-python@0.11.0',
-        'tm-reporter @ git+https://github.com/cms-l1-globaltrigger/tm-reporter@2.11.1'
+        'tm-python @ git+https://github.com/cms-l1-globaltrigger/tm-python@0.11.1',
+        'tm-reporter @ git+https://github.com/cms-l1-globaltrigger/tm-reporter@2.11.2'
     ],
     entry_points={
         'console_scripts': [
