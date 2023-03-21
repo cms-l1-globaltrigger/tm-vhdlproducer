6ee1792b2905c1797242ec1de01e152dffdb68ba
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-03-21 10:34:03 +0100

    added template

---
+++ tmVhdlProducer/templates/vhdl/instances/anomaly_detection/anomaly_detection_sig.vhd
@@ -0,0 +1,18 @@
+-- Anomaly detection instantiation
+{%- set o = condition.objects[0] %}
+cond_{{ condition.vhdl_signal }}_i: entity work.adt_wrapper
+    generic map(false, {{ o.anomalyScore.value }})
+    port map(
+        lhc_clk,
+        bx_data.mu(2),
+        bx_data.eg(2),
+        bx_data.jet(2),
+        bx_data.tau(2),
+        bx_data.ett(2),
+        bx_data.htt(2),
+        bx_data.etm(2),
+        bx_data.htm(2),
+        bx_data.etmhf(2),
+        {{ condition.vhdl_signal }}
+    );
+
