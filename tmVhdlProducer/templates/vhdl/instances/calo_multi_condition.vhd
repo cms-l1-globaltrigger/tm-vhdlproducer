e18ec82f302571c635ec11c05ccca633a9dd2986
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2024-05-24 08:59:25 +0200

    updated template and resource values

--- tmVhdlProducer/templates/vhdl/instances/calo_multi_condition.vhd
+++ tmVhdlProducer/templates/vhdl/instances/calo_multi_condition.vhd
@@ -1,13 +1,9 @@
 {%- set o1 = condition.objects[0] -%}
 
 cond_{{ condition.vhdl_signal }}_i: entity work.calo_comb_multi_condition
-    generic map(
-  {%- if not o1.operator -%}
-    pt_ge_mode => {{ o1.operator | vhdl_bool }},
-  {% endif %}
-        obj_nr => {{ condition.nr_objects }}, 
-        pt_width => {{ o1.type | upper }}_ET_BITS, 
-        pt_threshold => X"{{ o1.threshold | X04 }}"
-    )
+  {%- if not o1.operator %}
+    generic map({{ condition.nr_objects }}, {{ o1.type | upper }}_ET_BITS, X"{{ o1.threshold | X04 }}", {{ o1.operator | vhdl_bool }})
+  {%- else %}
+    generic map({{ condition.nr_objects }}, {{ o1.type | upper }}_ET_BITS, X"{{ o1.threshold | X04 }}")
+  {%- endif %}
     port map(lhc_clk, bx_data.{{ o1.type | lower }}({{ o1.bx_arr }}), {{ condition.vhdl_signal }});
-
