ed6fb3a0f16e07256ecf3286b2354cb47ddeec73
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2024-08-12 15:50:22 +0200

    added "htmhf" in template signal_condition.vhd for "AXO" and "TOPO"

--- tmVhdlProducer/templates/vhdl/instances/signal_condition.vhd
+++ tmVhdlProducer/templates/vhdl/instances/signal_condition.vhd
@@ -29,9 +29,7 @@ cond_{{ condition.vhdl_signal }}: entity work.axol1tl_{{ o.model.value }}_wrappe
         bx_data.etm({{ o.bx_arr }}),
         bx_data.htm({{ o.bx_arr }}),
         bx_data.etmhf({{ o.bx_arr }}),
-{%- if o.model.value != "v1" and o.model.value != "v3" %}
         bx_data.htmhf({{ o.bx_arr }}),
-{%- endif %}
         {{ condition.vhdl_signal }}
     );
 {%- elif o.type == "TOPO" -%}
@@ -48,6 +46,7 @@ cond_{{ condition.vhdl_signal }}: entity work.topo_{{ o.model.value }}_wrapper
         bx_data.etm({{ o.bx_arr }}),
         bx_data.htm({{ o.bx_arr }}),
         bx_data.etmhf({{ o.bx_arr }}),
+        bx_data.htmhf({{ o.bx_arr }}),
         {{ condition.vhdl_signal }}
     );
 {%- elif o.type == "CICADA" -%}
